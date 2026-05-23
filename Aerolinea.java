d488eba595257a847c27ce95559f411c757d916a
Sampg1012 <sportog@unicartagena.edu.co>
2026-05-23 13:59:34 -0500

    feed: add main

---
+++ Aerolinea.java
@@ -0,0 +1,25 @@
+public class Aerolinea {
+    private String nombre;
+    private String codigo;
+
+    public Aerolinea(String nombre, String codigo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
